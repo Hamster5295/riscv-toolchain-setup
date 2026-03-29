b685b7084561db58380d5ddace04a8088538302b
Hamster5295 <hamster5295@163.com>
2026-03-29 10:12:31 +0800

    fix: xz untar

--- src/main.ts
+++ src/main.ts
@@ -5,58 +5,58 @@ import * as os from "os";
 import * as path from "path";
 
 async function run(): Promise<void> {
-  try {
-    const version = core.getInput("version");
-    const arch = core.getInput("arch");
-    const libc = core.getInput("libc");
-    const compiler = core.getInput("compiler");
-    const os = core.getInput("os");
+    try {
+        const version = core.getInput("version");
+        const arch = core.getInput("arch");
+        const libc = core.getInput("libc");
+        const compiler = core.getInput("compiler");
+        const os = core.getInput("os");
 
-    core.info("Hello! I am going to setup riscv-collab/riscv-gnu-toolchain with the following configuration:");
-    core.info("version:  " + version);
-    core.info("arch:     " + arch);
-    core.info("libc:     " + libc);
-    core.info("compiler: " + compiler);
-    core.info("os:       " + os);
+        core.info("Hello! I am going to setup riscv-collab/riscv-gnu-toolchain with the following configuration:");
+        core.info("version:  " + version);
+        core.info("arch:     " + arch);
+        core.info("libc:     " + libc);
+        core.info("compiler: " + compiler);
+        core.info("os:       " + os);
 
-    // TODO: support parsing `latest` version to tag name
-    let url = `https://github.com/riscv-collab/riscv-gnu-toolchain/releases/download/${version}/${arch}-${libc}-${os}-${compiler}.tar.xz`
-    core.info("Downloading from " + url)
+        // TODO: support parsing `latest` version to tag name
+        let url = `https://github.com/riscv-collab/riscv-gnu-toolchain/releases/download/${version}/${arch}-${libc}-${os}-${compiler}.tar.xz`
+        core.info("Downloading from " + url)
 
-    const toolchainHome = await install(url);
-    core.info("Nice to meet you! This is");
-    await exec.exec(path.join(toolchainHome, "riscv", "bin", "riscv64-unknown-linux-gnu-gcc"), ["--version"]);
-    
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message);
-  }
+        const toolchainHome = await install(url);
+        core.info("Nice to meet you! This is");
+        await exec.exec(path.join(toolchainHome, "riscv", "bin", "riscv64-unknown-linux-gnu-gcc"), ["--version"]);
+
+    } catch (error) {
+        if (error instanceof Error) core.setFailed(error.message);
+    }
 }
 
 async function install(url: string): Promise<string> {
-  const os = detectOS();
-  if (os !== "linux") {
-    core.setFailed("Currently only Linux is supported");
-    throw new Error("Currently only Linux is supported");
-  }
+    const os = detectOS();
+    if (os !== "linux") {
+        core.setFailed("Currently only Linux is supported");
+        throw new Error("Currently only Linux is supported");
+    }
 
-  core.info("Downloading from " + url);
-  const file = await tool.downloadTool(url);
-  core.info("Downloaded to " + file);
+    core.info("Downloading from " + url);
+    const file = await tool.downloadTool(url);
+    core.info("Downloaded to " + file);
 
-  const toolchainHome = await tool.extractTar(file);
-  core.addPath(path.join(toolchainHome, "riscv", "bin"));
-  core.exportVariable("RISCV_HOME", path.join(toolchainHome, "riscv"));
-  core.exportVariable("RISCV_SYSROOT", path.join(toolchainHome, "riscv", "sysroot"));
-  return toolchainHome;
+    const toolchainHome = await tool.extractTar(file, "riscv-toolchain", "-xjf");
+    core.addPath(path.join(toolchainHome, "riscv", "bin"));
+    core.exportVariable("RISCV_HOME", path.join(toolchainHome, "riscv"));
+    core.exportVariable("RISCV_SYSROOT", path.join(toolchainHome, "riscv", "sysroot"));
+    return toolchainHome;
 }
 
 /** convert current OS to GitHub runner names */
 function detectOS(): string {
-  var osType: string = os.type().toLowerCase();
-  if (osType.includes("linux")) return "linux";
-  if (osType.includes("darwin")) return "macos";
-  if (osType.includes("windows")) return "windows";
-  throw new Error("Unknown OS: " + osType);
+    var osType: string = os.type().toLowerCase();
+    if (osType.includes("linux")) return "linux";
+    if (osType.includes("darwin")) return "macos";
+    if (osType.includes("windows")) return "windows";
+    throw new Error("Unknown OS: " + osType);
 }
 
 run()
